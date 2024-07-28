431b701733b79308134cfbf05973c7107c9fd6e6
DanielSurf10 <daniel101surf@gmail.com>
2024-07-28 19:04:42 -0300

    começando "menor distancia array"

---
+++ menor_distancia_array/menor_distancia_array.c
@@ -0,0 +1,29 @@
+/*
+Menor distância de dois arrays
+
+João estava participando de uma competição de programação e lhe foi dado um problema em que ele tinha
+que encontrar a menor distância entre dois números de dois arrays.
+
+Um exemplo seria:
+
+    Array 1 -> [-1, 5]
+    Array 2 -> [26, 6]
+
+A menor distância seria a combinação do número 5 do array 1 com o número 6 do array 2, que seria 1
+de distância.
+
+Instruções
+    Use arrays com tamanho maiores ou iguais a 10 números.
+
+    De preferência, coloque o código em um arquivo único, para que seja possível executar online e
+    coloque o link do site que executa. Ex: playcode.io/javascript
+*/
+
+// Instruções:
+// 1. Crie dois arrays com pelo menos 10 números cada.
+// 2. Compare todos os pares possíveis de números, um de cada array.
+// 3. Calcule a distância entre cada par de números.
+// 7. Certifique-se de usar arrays com tamanho maior ou igual a 10 números.
+
+// usando a fórmula: distância = |número1 - número2|.
+
